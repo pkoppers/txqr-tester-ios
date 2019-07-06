e75a6bec566cc792d927bda7b00bd016e382ef35
Patrick Koppers <1890707+pkoppers@users.noreply.github.com>
2019-07-06 11:15:34 +0200

    Added more camera-types in discovery-session.

--- txqr-tester/QRScannerController.swift
+++ txqr-tester/QRScannerController.swift
@@ -19,7 +19,7 @@ class QRScannerController: UIViewController {
         super.viewDidLoad()
 
         // Get the back-facing camera for capturing videos
-        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera, .builtInTelephotoCamera, .builtInDualCamera], mediaType: AVMediaType.video, position: .back)
         
         guard let captureDevice = deviceDiscoverySession.devices.first else {
             print("Failed to get the camera device")
